b58bd8bee63a9c318f7c8343c0b9b0a8e8ca42a3
yonso <yeoni8@gmail.com>
2015-12-23 23:22:12 +0200

    msg

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/researcher', function () {
+    return view('adminlte/adminlte');
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
