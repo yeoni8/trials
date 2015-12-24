063347eb3622e47f2e783f9088a331ef894193f5
yonso <yeoni8@gmail.com>
2015-12-24 13:55:50 +0200

    .

--- app/Http/Controllers/ResearcherController.php
+++ app/Http/Controllers/ResearcherController.php
@@ -8,14 +8,16 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use App\Researcher;
-
+use App\Trial;
 
 class ResearcherController extends Controller
 {
     public function index(){
 
+        return Trial::where('researcher_id','=',2)->get();
+
         return Researcher::all();
-        
+
     }
     //
 }
