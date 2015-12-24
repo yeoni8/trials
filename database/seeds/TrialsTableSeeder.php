c31b50c6c8c295df3227522c18c8e6bf1d5bf04f
yonso <yeoni8@gmail.com>
2015-12-24 13:57:39 +0200

    .

---
+++ database/seeds/TrialsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Trial;
+
+class TrialsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        for ($i=0; $i<10; $i++){
+            $t = new Trial();
+            $t->researcher_id = rand(1,4);
+            $t->name = 'a test ' . rand();
+            $t->save();
+    }
+
+
+        //
+    }
+}
